8b5e298f3014eb7997bda4cf73ffc2b3abc72ebb
BarczaD <barcza.d1@gmail.com>
2025-03-17 20:19:27 +0100

    prepared database connection parameters

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'sqlite:../database/database.db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
