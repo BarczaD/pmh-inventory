2484857018eff2f4a93be9d48df202b5dc63f36a
David Barcza <barcza.d1@gmail.com>
2025-06-16 15:44:24 +0200

    installed jui extension and implemented the maintenance add form

--- views/maintenance/_maintenanceForm.php
+++ views/maintenance/_maintenanceForm.php
@@ -2,6 +2,7 @@
 
 use app\controllers\WorkstationController;
 use yii\helpers\Html;
+use yii\jui\DatePicker;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use app\models\Brand;
@@ -13,6 +14,7 @@ use app\models\Office;
 /** @var yii\web\View $this */
 /** @var app\models\Workstation $model */
 /** @var yii\widgets\ActiveForm $form */
+date_default_timezone_set("Europe/Budapest");
 
 ?>
 
@@ -24,7 +26,7 @@ use app\models\Office;
     <div class="row">
         <div class="col-lg-4 mb-3">
             <div class="form-group">
-                <label class="form-label" for="maintenance-workstation_id">Munkaállomás</label>
+                <label class="form-label" for="maintenance-workstation_id">Munkaállomás Hostname</label>
                 <?= Html::activeDropDownList(
                     $model,
                     'workstation_id',
@@ -32,16 +34,33 @@ use app\models\Office;
                     ['class' => 'form-control', 'prompt' => 'Válaszd ki a Munkaállomást...']
                 ) ?>
             </div>
-
+        </div>
+        <div class="col-lg-6 mb-5">
             <div class="form-group">
-                <label class="form-label" for="maintenance-workstation_id">Munkaállomás</label>
-
+                <?= $form->field($model, 'hardware')->checkbox([
+                    'checked' => false,
+                    'uncheck' => 0,
+                    'label' => 'Hardware',
+                ]) ?>
+                <?= $form->field($model, 'software')->checkbox([
+                    'checked' => false,
+                    'uncheck' => 0,
+                    'label' => 'Software',
+                ]) ?>
             </div>
         </div>
 
+        <div class="cold-lg-8 mb-7">
+            <?= $form->field($model, 'date')->widget(DatePicker::className(), ['name'=> 'Dátum', 'clientOptions' => ['defaultDate' => 20 . date("yyyy-mm-dd")]]) ?>
+        </div>
+
+        <?= $form->field($model, 'description')->textarea(['rows' => 3])->label("Egyéb leírás") ?>
 
 
 
+        <div class="form-group">
+            <?= Html::submitButton('Beküldés', ['class' => 'btn btn-success']) ?>
+        </div>
 
 
         <?php ActiveForm::end(); ?>
